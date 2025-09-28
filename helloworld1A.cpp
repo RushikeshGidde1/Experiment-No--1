2b2b5dba0c2471592168c29acea52adf81720340
Rushikesh Gidde <rushikesh.gidde.btech2024@sitpune.edu.in>
2025-09-28 21:08:54 +0530

    Add files via upload

---
+++ helloworld1A.cpp
@@ -0,0 +1,18 @@
+// Name: RUSHIKESH GIDDE 
+// PRN: 25070123511
+// Experiment 1(A)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+/* Output
+
+Hello, World!
+
+*/
